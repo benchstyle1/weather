6b92919b3ea534c1db1f1e62923f5ec869eda9b2
benchstyle1 <47250962+canijke@users.noreply.github.com>
2020-07-20 10:39:34 +0300

    deploy

---
+++ precache-manifest.9ff122d706844df9e92396ea7ac67a7f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36d44c4541019c46a358",
+    "url": "/weather/css/app.bafba28e.css"
+  },
+  {
+    "revision": "f59cb3b3c242bcdaccde59f1e1aba061",
+    "url": "/weather/fonts/Epilogue.f59cb3b3.ttf"
+  },
+  {
+    "revision": "ee9791ebdedacb0d5ca7eec82b25a392",
+    "url": "/weather/img/background.ee9791eb.jpg"
+  },
+  {
+    "revision": "971914204c06dd90b8b22b936cf18d86",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "36d44c4541019c46a358",
+    "url": "/weather/js/app.7253067d.js"
+  },
+  {
+    "revision": "bb95ad98f07103c5c184",
+    "url": "/weather/js/chunk-vendors.d60b6f21.js"
+  },
+  {
+    "revision": "458234fc6be47cef7c9146de12cc7556",
+    "url": "/weather/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/weather/robots.txt"
+  }
+]);
